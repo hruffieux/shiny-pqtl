a8fce1cba8f4a4cec5d21ebb00cb72a74166b0ec
Radu Popescu <mail@radupopescu.net>
2018-11-19 22:04:30 +0100

    Disable streaming protocols in Shiny server

--- vm-setup/configure_shiny_server.sh
+++ vm-setup/configure_shiny_server.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
 
-ret=$(grep disable_websockets /etc/shiny-server/shiny-server.conf | wc -l)
+ret=$(grep "disable_websockets;" /etc/shiny-server/shiny-server.conf | wc -l)
 
 if [ $ret -eq 0 ]; then
     echo "disable_websockets;" >> /etc/shiny-server/shiny-server.conf
 fi
+
+ret=$(grep "disable_protocols xdr-streaming xhr-streaming iframe-eventsource iframe-htmlfile;" /etc/shiny-server/shiny-server.conf | wc -l)
+
+if [ $ret -eq 0 ]; then
+    echo "disable_protocols xdr-streaming xhr-streaming iframe-eventsource iframe-htmlfile;" >> /etc/shiny-server/shiny-server.conf
+fi
+
